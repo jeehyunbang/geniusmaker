9a31073499fa86ea74377b59f667c681fbdad196
kelly6bf <kelly6bf@gmail.com>
2025-02-11 21:06:26 +0900

    springboot 환경 생성

---
+++ genius_maker/src/main/java/com/example/genius_maker/GeniusMakerApplication.java
@@ -0,0 +1,13 @@
+package com.example.genius_maker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeniusMakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeniusMakerApplication.class, args);
+    }
+
+}
---
+++ genius_maker/src/test/java/com/example/genius_maker/GeniusMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.genius_maker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeniusMakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
