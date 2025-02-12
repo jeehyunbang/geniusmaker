e28685b192aec4a2084f21b3acb291337698879e
kelly6bf <kelly6bf@gmail.com>
2025-02-12 16:14:36 +0900

    기타 Entity 추가

---
+++ genius_maker/src/main/java/com/example/genius_maker/otherentity/ConferenceRepository.java
@@ -0,0 +1,6 @@
+package com.example.genius_maker.otherentity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConferenceRepository extends JpaRepository<Conference, Long> {
+}
